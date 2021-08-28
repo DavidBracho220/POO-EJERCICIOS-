826e19be976471c16fe7b2308c4656f9e46d51bb
DavidBracho220 <88252913+DavidBracho220@users.noreply.github.com>
2021-08-28 02:11:29 -0400

    Add files via upload

---
+++ Motocicleta y Camioneta.java
@@ -0,0 +1,22 @@
+package motocicleta;
+public class Motocicleta  
+{ 
+
+	private String matricula;
+	private String color; 
+	private int velocidad;
+	private boolean en_marcha; 
+	
+	public void arrancar ()
+     {} 
+    public void acelerar () 
+     {}
+     public void frenar () 
+     {} 
+     public void girar (float angulo) 
+     {}
+     
+ public static void main ()
+ {}
+
+} 
